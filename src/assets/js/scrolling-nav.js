d14ac892391e45dbddc6f4632c3744725eaf9a32
FrannieJames <ballenasfra.ne08@gmail.com>
2020-03-18 06:52:45 +0800

    set js

--- src/assets/js/scrolling-nav.js
+++ src/assets/js/scrolling-nav.js
@@ -1,14 +1,12 @@
-
-//jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
 
-    $('a.page-scroll[href*="#"]:not([href="#"])').on('click', function () {
+    $('a.page-scroll[href*="#"]:not([href="#"])').on('click', function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html, body').animate({
-                    scrollTop: (target.offset().top -70)
+                    scrollTop: (target.offset().top - 70)
                 }, 1200, "easeInOutExpo");
                 return false;
             }
@@ -16,4 +14,3 @@ $(function() {
     });
 
 });
-
